40d4260ccd6f3cca1523297d9553a83b4208b835
Glebus <glekar03@mail.ru>
2022-10-02 10:55:31 +0300

    add homework 1 task 2 solution

---
+++ Homeworks/Homework1/H1Task2/H1Task2Tests/BWTTests.cs
@@ -0,0 +1,46 @@
+using H1Task2;
+using NUnit.Framework;
+
+namespace H1Task2Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void DirectBWTTest()
+    {
+        const string example = "ABACABA";
+        var transformed = BurrowsWheelerTransformation.Transform(example);
+        Assert.Multiple(() =>
+        {
+            Assert.That(transformed.Item1, Is.EqualTo("BCABAAA"));
+            Assert.That(transformed.Item2, Is.EqualTo(2));
+        });
+    }
+
+    [Test]
+    public void ReverseBWTTest()
+    {
+        const string example = "BCABAAA";
+        const int exampleIndex = 2;
+        var reversed = BurrowsWheelerTransformation.ReverseTransform(example, exampleIndex);
+        Assert.That(reversed, Is.EqualTo("ABACABA"));
+    }
+
+    [Test]
+    [Repeat(10)]
+    public void NegativeDataTest()
+    {
+        var random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=";
+        var length = random.Next(10);
+        var stringBefore = new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        var transformed = BurrowsWheelerTransformation.Transform(stringBefore);
+        var reversed = BurrowsWheelerTransformation.ReverseTransform(transformed.Item1, transformed.Item2);
+        Assert.That(reversed, Is.EqualTo(stringBefore));
+    }
+}
